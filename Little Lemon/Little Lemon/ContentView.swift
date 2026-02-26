979d77559b8e406ba5ff546d0bc3914b2b27a155
William Tempero <wtempero@gmail.com>
2026-02-26 14:15:18 -0500

    Add .gitignore for Xcode and Swift project files

---
+++ Little Lemon/Little Lemon/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Little Lemon
+//
+//  Created by William Tempero on 2/26/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
